861fa5508c432af3cd3826ac7b71622f07e6921e
Huynh Thai Khiem <khiemht@bap.jp>
2023-04-26 14:09:07 +0700

    upload degree

--- src/screens/UploadDetails/index.js
+++ src/screens/UploadDetails/index.js
@@ -1,46 +1,23 @@
 import React, { useState } from "react";
 import cn from "classnames";
 import styles from "./UploadDetails.module.sass";
-import Dropdown from "../../components/Dropdown";
 import Icon from "../../components/Icon";
 import TextInput from "../../components/TextInput";
-import Switch from "../../components/Switch";
 import Loader from "../../components/Loader";
 import Modal from "../../components/Modal";
 import Preview from "./Preview";
-import Cards from "./Cards";
 import FolowSteps from "./FolowSteps";
 
-const royaltiesOptions = ["10%", "20%", "30%"];
-
-const items = [
-  {
-    title: "Create collection",
-    color: "#4BC9F0",
-  },
-  {
-    title: "Crypto Legend - Professor",
-    color: "#45B26B",
-  },
-  {
-    title: "Crypto Legend - Professor",
-    color: "#EF466F",
-  },
-  {
-    title: "Legend Photography",
-    color: "#9757D7",
-  },
-];
-
 const Upload = () => {
-  const [royalties, setRoyalties] = useState(royaltiesOptions[0]);
-  const [sale, setSale] = useState(true);
-  const [price, setPrice] = useState(false);
-  const [locking, setLocking] = useState(false);
-
   const [visibleModal, setVisibleModal] = useState(false);
 
   const [visiblePreview, setVisiblePreview] = useState(false);
+
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileSelect = (event) => {
+    setSelectedFile(URL.createObjectURL(event.target.files[0]));
+  };
 
   return (
     <>
@@ -48,30 +25,32 @@ const Upload = () => {
         <div className={cn("container", styles.container)}>
           <div className={styles.wrapper}>
             <div className={styles.head}>
-              <div className={cn("h2", styles.title)}>
-                Create single collectible
-              </div>
-              <button
-                className={cn("button-stroke button-small", styles.button)}
-              >
-                Switch to Multiple
-              </button>
+              <div className={cn("h2", styles.title)}>Create Diploma</div>
             </div>
             <form className={styles.form} action="">
               <div className={styles.list}>
                 <div className={styles.item}>
-                  <div className={styles.category}>Upload file</div>
+                  <div className={styles.category}>Upload Degree</div>
                   <div className={styles.note}>
                     Drag or choose your file to upload
                   </div>
                   <div className={styles.file}>
-                    <input className={styles.load} type="file" />
+                    <input
+                      className={styles.load}
+                      type="file"
+                      onChange={handleFileSelect}
+                    />
                     <div className={styles.icon}>
                       <Icon name="upload-file" size="24" />
                     </div>
-                    <div className={styles.format}>
-                      PNG, GIF, WEBP, MP4 or MP3. Max 1Gb.
-                    </div>
+                    <div className={styles.format}>PNG, PDF, JPG Max 1Gb.</div>
+                    {selectedFile && (
+                      <div className={styles.item}>
+                        <div className={styles.image}>
+                          <img src={selectedFile} alt="Selected" />
+                        </div>
+                      </div>
+                    )}
                   </div>
                 </div>
                 <div className={styles.item}>
@@ -79,10 +58,10 @@ const Upload = () => {
                   <div className={styles.fieldset}>
                     <TextInput
                       className={styles.field}
-                      label="Item name"
+                      label="Student name"
                       name="Item"
                       type="text"
-                      placeholder='e. g. Redeemable Bitcoin Card with logo"'
+                      placeholder="e. g. “Ho Ngoc Tam”"
                       required
                     />
                     <TextInput
@@ -90,78 +69,11 @@ const Upload = () => {
                       label="Description"
                       name="Description"
                       type="text"
-                      placeholder="e. g. “After purchasing you will able to recived the logo...”"
+                      placeholder="e. g. “20IT971”"
                       required
                     />
-                    <div className={styles.row}>
-                      <div className={styles.col}>
-                        <div className={styles.field}>
-                          <div className={styles.label}>Royalties</div>
-                          <Dropdown
-                            className={styles.dropdown}
-                            value={royalties}
-                            setValue={setRoyalties}
-                            options={royaltiesOptions}
-                          />
-                        </div>
-                      </div>
-                      <div className={styles.col}>
-                        <TextInput
-                          className={styles.field}
-                          label="Size"
-                          name="Size"
-                          type="text"
-                          placeholder="e. g. Size"
-                          required
-                        />
-                      </div>
-                      <div className={styles.col}>
-                        <TextInput
-                          className={styles.field}
-                          label="Propertie"
-                          name="Propertie"
-                          type="text"
-                          placeholder="e. g. Propertie"
-                          required
-                        />
-                      </div>
-                    </div>
                   </div>
                 </div>
-              </div>
-              <div className={styles.options}>
-                <div className={styles.option}>
-                  <div className={styles.box}>
-                    <div className={styles.category}>Put on sale</div>
-                    <div className={styles.text}>
-                      You’ll receive bids on this item
-                    </div>
-                  </div>
-                  <Switch value={sale} setValue={setSale} />
-                </div>
-                <div className={styles.option}>
-                  <div className={styles.box}>
-                    <div className={styles.category}>Instant sale price</div>
-                    <div className={styles.text}>
-                      Enter the price for which the item will be instantly sold
-                    </div>
-                  </div>
-                  <Switch value={price} setValue={setPrice} />
-                </div>
-                <div className={styles.option}>
-                  <div className={styles.box}>
-                    <div className={styles.category}>Unlock once purchased</div>
-                    <div className={styles.text}>
-                      Content will be unlocked after successful transaction
-                    </div>
-                  </div>
-                  <Switch value={locking} setValue={setLocking} />
-                </div>
-                <div className={styles.category}>Choose collection</div>
-                <div className={styles.text}>
-                  Choose an exiting collection or create a new one
-                </div>
-                <Cards className={styles.cards} items={items} />
               </div>
               <div className={styles.foot}>
                 <button
